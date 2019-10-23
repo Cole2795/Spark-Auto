adf03aae93351f42c88fc10597f61dfb553a23bb
Nicole Campbell <nicole.campbell@tekenable.com>
2019-10-23 17:13:35 +0100

    finished course

---
+++ BookListRazor/Spark2Auto/Data/DbInitializer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Spark2Auto.Models;
+using Spark2Auto.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spark2Auto.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> rolemanager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = rolemanager;
+        }
+        public async void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            if (_db.Roles.Any(r => r.Name == SD.AdminEndUser)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.AdminEndUser)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.CustomerEndUser)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                Name = "Nicole Campbell",
+                EmailConfirmed = true,
+                PhoneNumber = "22222222"
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            
+
+            _userManager.AddToRoleAsync(_db.Users.FirstOrDefaultAsync(u => u.Email == "admin@gmail.com").GetAwaiter().GetResult(), SD.AdminEndUser).GetAwaiter().GetResult();
+        }
+    }
+}
